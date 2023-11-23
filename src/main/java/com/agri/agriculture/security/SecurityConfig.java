bd03ef1b1339939356b279249859e8a4a2537f91
Ganesh Matsagar <145926912+GaneshMatsagar000@users.noreply.github.com>
2023-11-23 08:58:14 +0530

    Update SecurityConfig.java

--- src/main/java/com/agri/agriculture/security/SecurityConfig.java
+++ src/main/java/com/agri/agriculture/security/SecurityConfig.java
@@ -31,50 +31,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            	.antMatchers("/api/farmers/register").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/api/farmers/register").permitAll()
+                .antMatchers("/api/farmers/{id}").permitAll()
+                .antMatchers("/api/workrequests/all").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/api/drivers/register").permitAll()
                 .antMatchers("/index.html").permitAll()
-                .antMatchers("/public/**").permitAll() // Allow public access to resources under /public/**
-                .antMatchers(HttpMethod.POST, "/api/tractorowners/register").permitAll() // Allow registration without authentication
+                .antMatchers("/public/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/tractorowners/register").permitAll()
                 .antMatchers("/css/**", "/js/**").permitAll()
                 .antMatchers("/resources/**").permitAll()
-                .antMatchers("/api/**").authenticated()
-                .anyRequest().permitAll() 
-                
+                .antMatchers("/api/location").permitAll()
+                .antMatchers("/api/send-otp").permitAll()
+                .antMatchers("/api/verify-otp").permitAll()
+                .antMatchers("/api/farmers/all").permitAll()
+                .antMatchers("/api/farmers/profile/{username}").permitAll()
+                .anyRequest().permitAll()
                 .and()
             .httpBasic()
-        .and()
-        .formLogin()
-            .loginPage("/login") // Customize the login page URL if needed
-            .permitAll() // Allow access to the login page
             .and()
-        .logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .logoutSuccessUrl("/login") // Customize the logout success URL if needed
-            .and()
-        .csrf().disable(); // Disable CSRF protection for simplicity
-        }
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and()
+            .csrf().disable();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-  
-    
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("ganesh").password(passwordEncoder().encode("ganesh")).roles("USER");
-    }
-
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-            .username("ganesh")
-            .password(passwordEncoder().encode("ganesh"))
-            .roles("USER") // Add roles as needed
-            .build();
-        return new InMemoryUserDetailsManager(user);
     }
 }
